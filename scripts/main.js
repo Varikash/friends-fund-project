3f47cd9c7be08b47b6b26fcd9b79dfe2966cca5f
Nick Varikash <n.varicash@gmail.com>
2022-10-13 02:11:07 +0300

    style: сверстать header меню

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+/* ----------------------------Функции----------------------------------- */
+
+/**
+ * Открытие бургерного меню
+ * @param {htmlElement} element
+ */
+function burgerMenuOpen (element) {
+  element.classList.add('');
+}
+
+/**
+ * Закрытие бургерного меню
+ * @param {htmlElement} element 
+ */
+function burgerMenuClose (element) {
+  element.classList.remove('');
+}
