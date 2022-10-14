0de1cdc95c3fb7162c7fca56c2a274bf63cae185
Nick Varikash <n.varicash@gmail.com>
2022-10-14 15:44:19 +0300

    style: сверстать попап 1 уровня для бургер меню

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,37 @@
+const menuIcon = document.querySelector('.menu-icon');
+const closeBtn = document.querySelector('.popup-menu-1__close-btn');
+const popupOne = document.querySelector('.popup-menu-1');
+const mediaSize = document.body.clientWidth;
 
+menuIcon?.addEventListener('click', () => {
+  burgerMenuOpen(popupOne);
+})
 
+closeBtn?.addEventListener('click', () => {
+  burgerMenuClose(popupOne);
+})
 
-
-
-
-
-
-
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 1049) {
+    burgerMenuClose(popupOne);
+  }
+});
 
 /* ----------------------------Функции----------------------------------- */
 
 /**
  * Открытие бургерного меню
- * @param {htmlElement} element
  */
-function burgerMenuOpen (element) {
-  element.classList.add('');
+function burgerMenuOpen (popupElement) {
+  popupElement.classList.add('popup_active');
 }
 
 /**
  * Закрытие бургерного меню
- * @param {htmlElement} element 
  */
-function burgerMenuClose (element) {
-  element.classList.remove('');
+function burgerMenuClose (popupElement) {
+  popupElement.classList.remove('popup_active');
 }
+
+
+
