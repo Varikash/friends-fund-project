0eb5ca6f64c7d17fa9044c6c272603d1efe21a83
Nick Varikash <n.varicash@gmail.com>
2022-10-14 20:54:08 +0300

    style: сверстать бургерное меню 2 уровня

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 const menuIcon = document.querySelector('.menu-icon');
 const closeBtn = document.querySelector('.popup-menu-1__close-btn');
+const backwardBtn = document.querySelector('.popup-menu-2__backward');
+const secondLevelPopupOpener = document.querySelector('#secondLevel');
 const popupOne = document.querySelector('.popup-menu-1');
-const mediaSize = document.body.clientWidth;
+const popupTwo = document.querySelector('.popup-menu-2');
+
 
 menuIcon?.addEventListener('click', () => {
   burgerMenuOpen(popupOne);
@@ -11,9 +14,19 @@ closeBtn?.addEventListener('click', () => {
   burgerMenuClose(popupOne);
 })
 
+secondLevelPopupOpener?.addEventListener('click', () => {
+  burgerMenuOpen(popupTwo);
+})
+
+backwardBtn?.addEventListener('click', () => {
+  burgerMenuClose(popupTwo);
+  burgerMenuOpen(popupOne);
+})
+
 window.addEventListener('resize', () => {
   if (window.innerWidth > 1049) {
     burgerMenuClose(popupOne);
+    burgerMenuClose(popupTwo);
   }
 });
 
