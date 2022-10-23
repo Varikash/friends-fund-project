5d852f651252caa2daf457992267d88510a97fe2
Nick Varikash <n.varicash@gmail.com>
2022-10-23 22:07:33 +0300

    style: сверстать футер и бургер меню 1 и 2 уровня

--- scripts/donate.js
+++ scripts/donate.js
@@ -5,6 +5,38 @@ const userEmail = document?.querySelector('.contact__user-email');
 const userMessage = document?.querySelector('.contact__user-message');
 const userAgreement = document?.querySelector('.contact__check-user-agreement');
 
+const menuIcon = document.querySelector('.menu-icon');
+const closeBtn = document.querySelector('.popup-menu-1__close-btn');
+const backwardBtn = document.querySelector('.popup-menu-2__backward');
+const secondLevelPopupOpener = document.querySelector('#secondLevel');
+const popupOne = document.querySelector('.popup-menu-1');
+const popupTwo = document.querySelector('.popup-menu-2');
+
+
+menuIcon?.addEventListener('click', () => {
+  burgerMenuOpen(popupOne);
+})
+
+closeBtn?.addEventListener('click', () => {
+  burgerMenuClose(popupOne);
+})
+
+secondLevelPopupOpener?.addEventListener('click', () => {
+  burgerMenuOpen(popupTwo);
+})
+
+backwardBtn?.addEventListener('click', () => {
+  burgerMenuClose(popupTwo);
+  burgerMenuOpen(popupOne);
+})
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 1049) {
+    burgerMenuClose(popupOne);
+    burgerMenuClose(popupTwo);
+  }
+});
+
 const buttonContact = document?.querySelector('.contact__form-btn');
 
 function changingButtonContactState () {
@@ -30,3 +62,20 @@ contactForm?.addEventListener('click', function () {
 contactForm?.addEventListener('compositionend', function () {
   changingButtonContactState ()
 });
+
+
+/* ----------------------------Функции----------------------------------- */
+
+/**
+ * Открытие бургерного меню
+ */
+function burgerMenuOpen (popupElement) {
+  popupElement.classList.add('popup_active');
+}
+
+/**
+ * Закрытие бургерного меню
+ */
+function burgerMenuClose (popupElement) {
+  popupElement.classList.remove('popup_active');
+}
